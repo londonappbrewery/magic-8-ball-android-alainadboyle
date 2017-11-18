8a513048d9d67fd8df26efd162f9e5a6747443c0
alainadboyle <a.boyle1@lse.ac.uk>
2017-11-18 10:55:01 +0000

    Project Completed!

---
+++ app/src/main/java/com/londonappbrewery/magiceightball/AppCompatActivity.java
@@ -0,0 +1,12 @@
+package com.londonappbrewery.magiceightball;
+
+import android.os.Bundle;
+
+/**
+ * Created by Alaina on 11/18/2017.
+ */
+
+class AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+    }
+}
--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,43 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        Button AskButton;
+        AskButton = (Button) findViewById(R.id.Askbutton);
+        final ImageView ballDisplay = (ImageView) findViewById(R.id.imageView);
+        
+        final int[] ballArray = {R.drawable.ball1,
+        R.drawable.ball2,
+        R.drawable.ball3,
+        R.drawable.ball4,
+        R.drawable.ball5};
+
+        AskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Magic 8 Ball", "You rolled the ball!");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(4);
+
+                Log.d("Dicee", "The answer is" + number);
+
+                ballDisplay.setImageResource(ballArray[number]);
+            }
+        });
+}}
